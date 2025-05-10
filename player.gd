0d66ece42fddc46a1be656475ce345a1b7ec3cf5
Matthew Shwe <matthew.ewhs@gmail.com>
2025-05-09 21:27:10 -0400

    style edits

--- player.gd
+++ player.gd
@@ -1,13 +1,21 @@
 extends CharacterBody3D
 
-@export var gravity: float = 2
-@export var player_speed: float = 3
-@export var jump_power: float = 12
+signal player_interacted
 
-var player_velocity: Vector3 = Vector3.ZERO
+@export var gravity: float = 2.0
+@export var player_speed: float = 3.0
+@export var jump_power: float = 12.0
+
+var player_velocity := Vector3.ZERO
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		player_interacted.emit()
+
 
 func _physics_process(delta: float) -> void:
-	# direction of movement in the X axis
+	# Direction of movement in the X axis
 	var movement_direction: int = 0
 	if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_right"):
 		pass
@@ -16,7 +24,7 @@ func _physics_process(delta: float) -> void:
 	elif  Input.is_action_pressed("move_right"):
 		movement_direction = 1
 	
-	#Jump
+	# Jump
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		player_velocity.y = jump_power
 	
@@ -25,7 +33,6 @@ func _physics_process(delta: float) -> void:
 	# Fall
 	if not is_on_floor():
 		player_velocity.y -= gravity
-	
 	
 	velocity = player_velocity
 	move_and_slide()
