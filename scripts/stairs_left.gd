9df343f966da3ef235241240e4342573c8c0521d
shquam <matthew.ewhs@gmail.com>
2025-05-16 00:08:14 -0400

    modified stairs

--- scripts/stairs_left.gd
+++ scripts/stairs_left.gd
@@ -1,14 +1,24 @@
+## Body entered functions detect when player walks to the top or bottom of the stairs
+## then move the player to the respective exit location
+##          should only trigger when the player walks into them while on the stairs
+
+## Player interacted functions move the player onto the stairs when interacting
 extends StaticBody3D
 
 
-func _physics_process(_delta: float) -> void:
-	if $PlayerEntranceDetector.has_overlapping_bodies():
-		var player: CharacterBody3D = $PlayerEntranceDetector.get_overlapping_bodies()[0]
-		if Input.is_action_just_pressed("move_up") and round(player.global_position.z) != -1:
-			player.global_position = $PlayerEntrancePoint.global_position
-		if Input.is_action_just_pressed("move_down") and round(player.global_position.z) != 0:
-			player.global_position = $PlayerEntranceReturnPoint.global_position
+func _on_interaction_detector_top_player_interacted(body: CharacterBody3D) -> void:
+	body.global_position = $TopEntrance.global_position
 
-	if $PlayerExitDetector.has_overlapping_bodies():
-		var player: CharacterBody3D = $PlayerExitDetector.get_overlapping_bodies()[0]
-		player.global_position = $PlayerExitPoint.global_position
+
+func _on_interaction_detector_top_body_entered(body: Node3D) -> void:
+	if body.global_position.z == global_position.z:
+		body.global_position = $TopReturnPoint.global_position
+
+
+func _on_interaction_detector_bottom_player_interacted(body: CharacterBody3D) -> void:
+	body.global_position = $BottomEntrance.global_position
+
+
+func _on_interaction_detector_bottom_body_entered(body: Node3D) -> void:
+	if body.global_position.z == global_position.z:
+		body.global_position = $BottomReturnPoint.global_position
